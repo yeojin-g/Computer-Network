428f3db63e921e70800f17da3a852e08ec5dde52
yeojin Goo <alina059900@gmail.com>
2024-04-30 00:26:05 +0900

    1차 완성본

--- hw.2/20212956.py
+++ hw.2/20212956.py
@@ -1,8 +1,7 @@
-# Student ID : 20212956 
-# Name : 구여진
-    
 from socket import *
+import os
 import sys
+import mimetypes
 
 def main():
     if len(sys.argv) != 2:
@@ -15,16 +14,41 @@ def main():
     print("Name : Yeojin Goo")
 
     serverPort = portnum
-    serverSocket = socket(AF_INET,SOCK_STREAM)
-    serverSocket.bind(('localhost',serverPort))
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('localhost', serverPort))
     serverSocket.listen(1)
 
     while True:
         connectionSocket, addr = serverSocket.accept()
         requestM = connectionSocket.recv(3000).decode('utf-8').split('\r\n')
-        #print(requestM[0])
-        #print(requestM[1])
-        print(requestM)
         
+        requestline = requestM[0]
+        userAgent = 0
+        for a in requestM:
+            if 'User-Agent' in a:
+                userAgent = a
+                break
+        print(requestline)
+        print(userAgent)
+        print(str(len(requestM)-3) + " headers")  
+        
+        fileName = requestline.split(' ')[1][1:]
+        
+        if not os.path.exists(fileName):
+           print(f"Server Error : No such file ./{fileName}!")
+           print("HTTP/1.0 404 NOT FOUND\nConnection: close\nContent-Length: 0\nContent-Type: text/html") 
+           continue
+            
+        with open(fileName, 'rb') as f:
+            data = f.read()
+            length = len(data)
+            cType = mimetypes.guess_type(fileName)[0]
+            respondM = f"HTTP/1.0 200 OK\nConnection: close\nContent-Length: {length}\nContent-Type: {cType}\n\n"
+            print(respondM)
+            connectionSocket.send(respondM.encode())
+            connectionSocket.send(data)
+        
+
 if __name__ == "__main__":
     main()
+

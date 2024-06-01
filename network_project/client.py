6c07a4df5115881a8d0226ed9f6bb60710ac5f4b
Yeojin Goo <alina059900@gmail.com>
2024-06-01 12:56:39 +0900

    .

--- network_project/client.py
+++ network_project/client.py
@@ -1,40 +1,69 @@
 import socket
 from threading import Thread
+import time
+import socketserver
 
-def recvMsg(sock):
+class clientServerHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        pass
+    
+class clientChatServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+
+def recvMsg(sock, users):
     while True:
-        msg = sock.recv(1024)
-        print(msg.decode())
+        try:
+            msg = sock.recv(1024).decode()
+            if msg:
+                print(msg)
+                if msg.startswith("<현재 접속 중인 User List>"):
+                    parse_user_list(msg, users)
+            else:
+                break
+        except:
+            break
 
-# 서버에 로그인
-def login_to_server(server_ip, server_port):
+def parse_user_list(msg, users):
+    lines = msg.split('\n')[1:]  # 첫 번째 라인은 "<현재 접속 중인 User List>"이므로 제외
+    for line in lines:
+        if line.startswith("Id:"):
+            nickname = line.split(": ")[1]
+        elif line.startswith("IP:"):
+            ip = line.split(": ")[1]
+        elif line.startswith("Port:"):
+            port = int(line.split(": ")[1])
+            users[nickname] = (ip, port)
+
+def login_to_server(server_ip, server_port, users, clientChat):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((server_ip, server_port))
-
+    
     while True:
         msg = sock.recv(1024).decode()
         print(msg)
         if "닉네임을 입력하세요" in msg:
             nickname = input("=> ")
-            sock.send(nickname.encode())  # 닉네임을 서버로 전송
-            break  # 닉네임을 입력한 후에는 반복문을 빠져나옵니다.
-
-    recv_thread = Thread(target=recvMsg, args=(sock,))
+            sock.send(nickname.encode())
+        elif "환영합니다." in msg:
+            break
+    
+    recv_thread = Thread(target=recvMsg, args=(sock, users))
     recv_thread.daemon = True
     recv_thread.start()
-
-    return sock
+    return nickname
 
 # 사용자 간 직접 통신을 위한 함수
-def direct_chat_with_user(sock, users, recipient, message):
-    if recipient in users:
-        ip, port = users[recipient]
+def direct_chat_with_user(users, nickname, message):
+    if nickname in users:
+        ip, port = users[nickname]
         # 소켓 생성
         user_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         user_sock.connect((ip, port))
 
         # 메시지 전송
-        send_msg = f"{recipient} {message}"
+        send_msg = f"{nickname}: {message}"
         user_sock.send(send_msg.encode())
         user_sock.close()  # 전송 후 소켓 닫기
     else:
@@ -42,15 +71,23 @@ def direct_chat_with_user(sock, users, recipient, message):
 
 if __name__ == "__main__":
     # 서버에 로그인
-    sock = login_to_server("localhost", 8000)
-    users = {"alina": ("127.0.0.1", 12345), "annie": ("127.0.0.1", 12346)}  # 사용자 목록을 담을 딕셔너리
-
+    users = {}  # 사용자 목록을 담을 딕셔너리
+    clientChat = None
+    nickname = login_to_server("localhost", 8001, users, clientChat)
+    time.sleep(0.5)
+    
+    ip, port = users[nickname]
+    clientChat = clientChatServer(("localhost", port), clientServerHandler)
+    clientChat.serve_forever()
+    
     while True:
         command = input("명령어를 입력하세요: ")
         if command.startswith("/send"):
-            _, recipient, message = command.split(" ", 2)
-            direct_chat_with_user(sock, users, recipient, message)
+            _, nickname, message = command.split(" ", 2)
+            direct_chat_with_user(users, nickname, message)
         elif command == "/exit":
             break
         else:
             print("잘못된 명령입니다.")
+
+    clientChat.server_close()
--- network_project/server.py
+++ network_project/server.py
@@ -3,40 +3,47 @@ import socketserver
 class ServerHandler(socketserver.BaseRequestHandler):
     # 접속 유저 관리
     users = {}
+    clients = []
 
     def readUserFile(self):
         try:
             with open("onlineUsers.txt", 'r') as f:
                 return f.read()
         except FileNotFoundError:
-            return ""
+            return "파일을 찾을 수 없습니다."
     
+    def updateUsers(self):
+        for i in self.clients:
+            userList = "\n\n                                                                                                                                                                                                                                                                                                          <현재 접속 중인 User List>\n" + self.readUserFile()
+            i.send(userList.encode())
+        
     def writeUserFile(self):
         with open("onlineUsers.txt", 'w') as f:
             for nickname, (ip, port) in self.users.items():
-                userData = f"Id: {nickname}\nIP: {ip}\nPort: {port}"
-                f.write(userData + '\n\n')
+                userData = f"Id: {nickname}\nIP: {ip}\nPort: {port}\n"
+                f.write(userData + '\n')
     
     def handle(self):
         ip, port = self.client_address
         print("클라이언트 연결")
-
-        self.request.send("닉네임을 입력하세요".encode())
-        nickname = self.request.recv(1024).decode().strip()
-        if not nickname:
-            self.request.close()
-            return
-
-        while nickname in self.users:
-            self.request.send("이미 사용 중인 닉네임입니다. 다른 닉네임을 입력하세요: ".encode())
+        nickname = "" 
+              
+        while True:
+            self.request.send("닉네임을 입력하세요".encode())
             nickname = self.request.recv(1024).decode().strip()
-            if not nickname:
-                self.request.close()
-                return
-
+            if nickname in self.users:
+                self.request.send("이미 사용 중인 닉네임입니다. 다른 닉네임을 입력하세요: ".encode())
+            elif not nickname:
+                self.request.send("올바른 닉네임을 입력해주세요.".encode())
+            else:
+                self.request.send(f"환영합니다. {nickname}님".encode())
+                break 
+            
         self.users[nickname] = (ip, port)
         self.writeUserFile()  # 새로운 사용자가 추가되면 파일에 새로 쓰기
-        userList = "< 현재 접속 중인 User List >\n" + self.readUserFile()
+        self.updateUsers()
+        self.clients.append(self.request) # 사용자 socket저장
+        userList = "<현재 접속 중인 User List>\n" + self.readUserFile()
         self.request.send(userList.encode())
 
         while True:
@@ -56,5 +63,6 @@ class ChatServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 if __name__ == "__main__":
     print("서버 실행")
-    chat = ChatServer(("", 8000), ServerHandler)
+    chat = ChatServer(("", 8001), ServerHandler)
     chat.serve_forever()
+    chat.server_close()
